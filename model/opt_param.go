ee0f784ea1d462a54210b3e98a2aeed362f9c7ea
Neniel <albertoojeda@live.com.ar>
2023-02-19 14:44:12 -0300

    refactor opt_param.go

--- model/opt_param.go
+++ model/opt_param.go
@@ -8,6 +8,18 @@ const (
 	ParamEscapedUnicode = "escapedUnicode"
 )
 
+type OptParam struct {
+	Key   string
+	Value interface{}
+}
+
+func NewOptParam(key string, value interface{}) *OptParam {
+	return &OptParam{
+		Key:   key,
+		Value: value,
+	}
+}
+
 var optParamsByPriority = []string{ParamEmail, ParamDomain, ParamIpAddress}
 
 type OptParams map[string]interface{}
@@ -22,42 +34,10 @@ func (p OptParams) GetHighPriorityParam() *OptParam {
 	return nil
 }
 
-type OptParam struct {
-	Key   string
-	Value interface{}
-}
-
-func NewOptParam(key string, value interface{}) *OptParam {
-	return &OptParam{
-		Key:   key,
-		Value: value,
+func (p OptParams) GetParam(key string) *OptParam {
+	if v, ok := p[key]; ok {
+		return NewOptParam(key, v)
 	}
-}
 
-func NewParamIPAddress(value string) *OptParam {
-	return &OptParam{
-		Key:   ParamIpAddress,
-		Value: value,
-	}
-}
-
-func NewParamDomain(value string) *OptParam {
-	return &OptParam{
-		Key:   ParamDomain,
-		Value: value,
-	}
-}
-
-func NewParamEmail(value string) *OptParam {
-	return &OptParam{
-		Key:   ParamEmail,
-		Value: value,
-	}
-}
-
-func NewParamEscapedUnicode(value uint8) *OptParam {
-	return &OptParam{
-		Key:   ParamEscapedUnicode,
-		Value: value,
-	}
+	return nil
 }
